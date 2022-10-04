0d27e3967c1c5cadbbeb0ff566e1bd45007ba28d
EideardHJZ <rooteruser610824@gmail.com>
2022-10-04 19:57:13 +0800

    109360122_HW2

---
+++ src/P25.java
@@ -0,0 +1,7 @@
+
+public class P25 {
+	public static void main(String [] args) {
+		int num = 3;
+		System.out.println("NUMªº­È¬°:"+num);
+	}
+}
---
+++ src/P9.java
@@ -0,0 +1,7 @@
+
+public class P9 {
+	public static void main(String[] args) {
+		System.out.print("歡迎使用JAVA");
+		System.out.print("學號:109360122");
+	}
+}
